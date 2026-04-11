57400433429f88e6fef914f18e819f9352cc00e1
Mohamed Ramadan <mramadan32@ymail.com>
2026-04-11 11:04:31 +0200

    First commit

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+import Button from "@/Components/UiElements/Button";
+
+import classes from "./page.module.css";
+
+export default function BlogPage() {
+  const [counter, setCounter] = useState(0);
+  const [showList, setShowList] = useState(true);
+
+  const increaseCounter = () => {
+    if (counter >= 10) return;
+
+    setCounter(counter + 1);
+  };
+
+  const decreaseCounter = () => {
+    if (counter <= 0) return;
+
+    setCounter(counter - 1);
+  };
+
+  const resetCounter = () => setCounter(0);
+
+  const toggleMenu = () => setShowList(!showList);
+
+  return (
+    <section>
+      <h2>Blog Page</h2>
+      <div>
+        <p>{counter}</p>
+
+        <section className={classes["actions"]}>
+          <Button onClick={increaseCounter} disabled={counter >= 10}>
+            +
+          </Button>
+          <Button onClick={decreaseCounter} outline disabled={counter <= 0}>
+            -
+          </Button>
+          <Button onClick={resetCounter} danger disabled={counter === 0}>
+            Reset
+          </Button>
+        </section>
+      </div>
+
+      <br />
+      <hr />
+      <br />
+
+      <div>
+        <Button onClick={toggleMenu} danger={showList}>
+          {showList ? "Hide" : "Show"} Menu
+        </Button>
+
+        <ul
+          className={`${classes["list"]} ${
+            !showList ? classes["hide-list"] : ""
+          }`}
+        >
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/results/[grade]/page.jsx
@@ -0,0 +1,10 @@
+import Results from "@/Components/Results/Results";
+import { notFound } from "next/navigation";
+
+export default async function GradePage({ params }) {
+  const { grade } = await params;
+
+  if (grade < 1 || grade > 3) notFound();
+
+  return <Results grade={grade} />;
+}
