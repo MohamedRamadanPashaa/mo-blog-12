e26ec3733350fd6b1b48fd2a0741fb52a050c113
Mohamed Ramadan <mramadan32@ymail.com>
2026-07-07 17:54:24 +0300

    Improve sign up page

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { isEmail, isPast, minLength } from "@/helpers/validators";
-import { useState } from "react";
+import { isEmail, isPast, minLength, theSame } from "@/helpers/validators";
+import { useMemo, useState } from "react";
+import Input from "../UiElements/Input";
+import Button from "../UiElements/Button";
 
-const formValidators = { name: minLength, email: isEmail, birthdate: isPast };
+const formValidators = {
+  name: minLength,
+  email: isEmail,
+  birthdate: isPast,
+  password: minLength,
+  passwordConfirm: theSame,
+};
 
 const SignUp = () => {
   // name email birthdate password passwordConfirm
@@ -11,6 +19,8 @@ const SignUp = () => {
     name: { value: "", isValid: false, touched: false },
     email: { value: "", isValid: false, touched: false },
     birthdate: { value: "", isValid: false, touched: false },
+    password: { value: "", isValid: false, touched: false },
+    passwordConfirm: { value: "", isValid: false, touched: false },
   });
 
   const handleInputChange = (e) => {
@@ -21,7 +31,11 @@ const SignUp = () => {
       [name]: {
         ...prev[name],
         value,
-        isValid: formValidators[name](value, 3),
+        isValid: formValidators[name]({
+          value,
+          value2: prev.password.value,
+          min: name === "name" ? 3 : name === "password" ? 6 : 0,
+        }),
       },
     }));
   };
@@ -37,61 +51,77 @@ const SignUp = () => {
 
   // console.log(formState);
 
+  const formIsValid = useMemo(
+    () => Object.keys(formState).every((el) => formState[el].isValid),
+    [formState],
+  );
+  console.log(formIsValid);
+
   return (
     <form>
       <h3>Create New Account</h3>
-      <div>
-        <label htmlFor="name">Full Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Write your full name"
-          value={formState.name.value}
-          onChange={handleInputChange}
-          onBlur={handleInputTouch}
-        />
-        <p>
-          {!formState.name.isValid && formState.name.touched
-            ? "Name should be at least 3 chars"
-            : ""}
-        </p>
-      </div>
+      <Input
+        id="name"
+        type="text"
+        name="name"
+        label="Full Name"
+        placeholder="Write your full name"
+        errorText="Name should be at least 3 chars"
+        inputState={formState.name}
+        onChange={handleInputChange}
+        onBlur={handleInputTouch}
+      />
 
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Write your email"
-          value={formState.email.value}
-          onChange={handleInputChange}
-          onBlur={handleInputTouch}
-        />
-        <p>
-          {!formState.email.isValid && formState.email.touched
-            ? "Please provide a valid email"
-            : ""}
-        </p>
-      </div>
+      <Input
+        id="email"
+        type="email"
+        name="email"
+        label="Your Email"
+        placeholder="write an exist email"
+        errorText="Please provide a valid email"
+        inputState={formState.email}
+        onChange={handleInputChange}
+        onBlur={handleInputTouch}
+      />
 
-      <div>
-        <label htmlFor="birthdate">Birthdate</label>
-        <input
-          type="date"
-          id="birthdate"
-          name="birthdate"
-          value={formState.birthdate.value}
-          onChange={handleInputChange}
-          onBlur={handleInputTouch}
-        />
-        <p>
-          {!formState.birthdate.isValid && formState.birthdate.touched
-            ? "Please provide a valid birthdate"
-            : ""}
-        </p>
-      </div>
+      <Input
+        id="birthdate"
+        type="date"
+        name="birthdate"
+        label="Birthdate"
+        errorText="Please provide a valid birthdate"
+        inputState={formState.birthdate}
+        onChange={handleInputChange}
+        onBlur={handleInputTouch}
+      />
+
+      <Input
+        id="password"
+        type="password"
+        name="password"
+        label="Password"
+        errorText="Password should be at least 6 chars"
+        placeholder="***********"
+        inputState={formState.password}
+        onChange={handleInputChange}
+        onBlur={handleInputTouch}
+      />
+
+      <Input
+        id="passwordConfirm"
+        type="password"
+        name="passwordConfirm"
+        label="Password Confirm"
+        errorText="Passwords are not the same"
+        placeholder="***********"
+        inputState={formState.passwordConfirm}
+        onChange={handleInputChange}
+        onBlur={handleInputTouch}
+      />
+
+      <Button disabled={!formIsValid} onClick={() => {}}>
+        Sign Up
+      </Button>
     </form>
   );
 };
